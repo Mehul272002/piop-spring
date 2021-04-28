faeb3e1432f9e329b93ed8dda26e4567c31313dd
mehul <mehul@Mehul-PC>
2021-04-28 12:30:13 +0530

    base for our poip project

---
+++ src/main/java/com/codespring/poip/PoipApplication.java
@@ -0,0 +1,13 @@
+package com.codespring.poip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codespring/poip/PoipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codespring.poip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
