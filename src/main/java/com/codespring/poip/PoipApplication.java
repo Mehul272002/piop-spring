6d50cdd0874eb3832fbd2a256e9148f76fe9e1ca
root <root@192.168.1.11>
2021-04-29 00:01:49 +0530

    Changes Made -
    1)Added db properties in application.properties
    2)Created table structure for table users
    3)Created testCreateUser function to test db & insert values
    4)Create UserRepository.java interface
    5)Created Getter& Setter methods for all the columns in the db

--- src/main/java/com/codespring/poip/PoipApplication.java
+++ src/main/java/com/codespring/poip/PoipApplication.java
@@ -3,6 +3,8 @@ package com.codespring.poip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class PoipApplication {
 
